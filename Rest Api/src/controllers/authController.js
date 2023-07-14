dd327db6538eee82b479f25b8500f5a3064741e9
Simona <simonapalazova@gmail.com>
2023-07-14 10:56:08 +0300

    create register functionality

--- Rest Api/src/controllers/authController.js
+++ Rest Api/src/controllers/authController.js
@@ -1,7 +1,31 @@
 const User = require('../models/User');
+
 const bcrypt = require('bcrypt');
 const jwt = require('../lib/jwt');
+
 const { SECRET, TOKEN_KEY } = require('../config/untilConfig');
+exports.register = async(req, res) => {
+    try {
+
+        const { username, email, password, repeatPassword } = req.body;
+        const user = await User.findOne({username});
+
+        if (user) {
+            return res.status(409).send("User Already Exist. Please Login")
+        }
+        console.log('hi');
+        const createdUser = await User.create({ username, email, password, repeatPassword });
+
+        const token = await generationToken(createdUser);
+        res.cookie(TOKEN_KEY, token);
+
+        createdUser.token = token;
+        res.status(201).json({ message: "Successfully Registered", status: 201 }).send(createdUser);
+    } catch (err) {
+        console.log(err);
+    }
+
+}
 async function generationToken(user) {
     const payload = {
         _id: user._id,
@@ -13,3 +37,4 @@ async function generationToken(user) {
 
     return token
 }
+
