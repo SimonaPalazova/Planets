d47bacfd9949cef5f8d63c5d4e6c341a198ee45d
Simona <simonapalazova@gmail.com>
2023-07-14 10:39:21 +0300

    add db to app

--- Rest Api/src/index.js
+++ Rest Api/src/index.js
@@ -1,5 +1,16 @@
 global.__basedir = __dirname;
 require('dotenv').config();
 const app = require('express')();
+const dbConnection = require('./config/dbConfig')
+
+
 require('./config/express')(app);
+
+
+dbConnection()
+    .then(() => console.log('DB Connected successfully!'))
+    .catch(err => console.log('DB error:', err))
+
 app.listen(3000, console.log('Listening on port 3000! Now its up to you...'));
+
+
