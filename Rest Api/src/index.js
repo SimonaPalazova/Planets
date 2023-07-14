a13537fa5f14b5977583b1a50e665332be536d8f
Simona <simonapalazova@gmail.com>
2023-07-14 10:44:09 +0300

    create and set errorHandler midleware

--- Rest Api/src/index.js
+++ Rest Api/src/index.js
@@ -2,10 +2,14 @@ global.__basedir = __dirname;
 require('dotenv').config();
 const app = require('express')();
 const dbConnection = require('./config/dbConfig')
+const apiRouter = require('./routes/index');
 
+//const { errorHandler } = require('./untils/errHandler');
 
 require('./config/express')(app);
+app.use('/user', apiRouter);
 
+//app.use(errorHandler);
 
 dbConnection()
     .then(() => console.log('DB Connected successfully!'))
