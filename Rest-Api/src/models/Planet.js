73ceb02bf2b9ee5e6ee68034371c4fc710b10d3d
Simona <simonapalazova@gmail.com>
2023-07-16 14:20:18 +0300

    create Planet model

---
+++ Rest-Api/src/models/Planet.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const planetSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true],
+    },
+    image:{
+        type: String,
+        required: [true],
+      
+    },
+    planetType:{
+        type: String,
+        required: [true],
+    },
+    overview:{
+        type: String,
+        required: [true],
+    },
+   
+    owner:{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    subscribe:{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }
+    
+});
