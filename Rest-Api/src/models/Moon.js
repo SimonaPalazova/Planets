0523970f8a8e8786b95377905f730ee7b2a2726f
Simona <simonapalazova@gmail.com>
2023-07-16 14:38:38 +0300

    create Moon model

---
+++ Rest-Api/src/models/Moon.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const moonSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true],
+    },
+    image:{
+        type: String,
+        required: [true],
+      
+    },
+    overview:{
+        type: String,
+        required: [true],
+    },
+   
+    owner:{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    subscribe:{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }
+    
+});
+const Moon = mongoose.model('Moon', moonSchema);
+module.exports = Moon;
