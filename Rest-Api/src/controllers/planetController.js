4bd0310f5649f3aa5aa79d14fb96089e7dff5ab8
Simona <simonapalazova@gmail.com>
2023-07-17 14:29:01 +0300

    create editPlanet functionality

--- Rest-Api/src/controllers/planetController.js
+++ Rest-Api/src/controllers/planetController.js
@@ -15,8 +15,10 @@ exports.getPlanet = async (req, res, next) => {
     const planetId = req.params.planetId;
     try {
         const planet = await Planet.findById(themeId)
+        const planet = await Planet.findById(planetId)
             .populate({
                 path: 'posts',
+                path: 'planets',
                 populate: {
                     path: 'userId'
                 }
@@ -35,12 +37,32 @@ exports.createPlanet = async (req, res, next) => {
     try {
         const createdPlanet = await Planet.create({ name, image, PlanetType, overview, subscribers: [userId] })
         res.status(201).json({ message: "Created!" })
+        res.json(createdPlanet);
     } catch (err) {
         console.log(err);
         next();
     }
 
 }
+exports.editPlanet = async(req, res, next) => {
+    const planetId = req.params.planetId
+    const { name, image, PlanetType, overview } = req.body;
+    const userId = req.user._id;
+    // if the userId is not the same as this one of the post, the post will not be updated
+    try{
+        const editedPlanet = await Planet.findOneAndUpdate({ _id: planetId, userId }, { name, image, PlanetType, overview }, { new: true }) 
+        if (editedPlanet) {
+                res.status(200).json(editedPlanet);
+            }
+            else {
+                res.status(401).json({ message: `Not allowed!` });
+            }
+    }catch (err) {
+        console.log(err);
+        next();
+    }
+}
+
 
 exports.subscribe = async (req, res, next) => {
     const planetId = req.params.planetId;
