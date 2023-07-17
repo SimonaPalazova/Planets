27793914c25adb315e4bcb8d742ecea7e5494c7f
Simona <simonapalazova@gmail.com>
2023-07-17 14:44:35 +0300

    Update create.js

--- Rest-Api/src/routes/create.js
+++ Rest-Api/src/routes/create.js
@@ -3,7 +3,7 @@ const { auth } = require('../utils/auth');
 const { createPlanet } = require('../controllers/planetController');
 const { createMoon } = require('../controllers/moonController');
 
-router.post('/moon', createMoon);
+router.post('/moon', auth, createMoon);
 router.post('/planet',auth, createPlanet)
 
 module.exports = router;
