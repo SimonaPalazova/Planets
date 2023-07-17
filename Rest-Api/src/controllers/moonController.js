b1a7350c531cfefa1ecda1b6f84e9d70a9ae838c
Simona <simonapalazova@gmail.com>
2023-07-17 14:25:05 +0300

    create getOneMoon functionality

--- Rest-Api/src/controllers/moonController.js
+++ Rest-Api/src/controllers/moonController.js
@@ -11,3 +11,21 @@ exports.getMoons = async (req, res, next) => {
 
 }
 
+exports.getMoon = async (req, res, next) => {
+    const moondId = req.params.moondId;
+    try {
+        const moon = await Moon.findById(moondId)
+            .populate({
+                path: 'moons',
+                populate: {
+                    path: 'userId'
+                }
+            })
+        res.json(moon);
+    } catch (err) {
+        console.log(err);
+        next();
+    }
+
+}
+
