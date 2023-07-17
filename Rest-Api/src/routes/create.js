75ab9f6c13aaf1d1701f840a681ec5f9bf2e656f
Simona <simonapalazova@gmail.com>
2023-07-17 14:32:20 +0300

    create routes and paths

---
+++ Rest-Api/src/routes/create.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const { auth } = require('../utils/auth');
+const { createPlanet } = require('../controllers/planetController');
+const { createMoon } = require('../controllers/moonController');
+
+router.post('/moon', createMoon);
+router.post('/planet',auth, createPlanet)
+
+module.exports = router;
