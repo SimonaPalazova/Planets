bca85989b523d62bba535adb30e12d7405a3ac4f
Simona <simonapalazova@gmail.com>
2023-07-24 10:55:09 +0300

    add home component

--- planets-app/src/app/app.module.ts
+++ planets-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 import { MainComponent } from './main/main.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     MainComponent
   ],
   imports: [
