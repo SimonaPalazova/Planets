53e31728d2ab0b197e54e594f5f9c4fe03702bc5
Simona <simonapalazova@gmail.com>
2023-07-27 14:50:34 +0300

    add routing module

---
+++ planets-app/src/app/moon/moon-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MoonListComponent } from '../moon-list/moon-list.component';
+import { OneMoonComponent } from './one-moon/one-moon.component';
+import { CreateMoonComponent } from './create-moon/create-moon.component';
+
+
+
+const routes: Routes = [
+  {
+    path: 'moons',
+    component: MoonListComponent
+  },
+  {
+    path: 'moons/:moonid',
+    component: OneMoonComponent
+  },
+  {
+    path: 'create/moon',
+    component: CreateMoonComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class MoonRoutingModule { }
--- planets-app/src/app/moon/moon.module.ts
+++ planets-app/src/app/moon/moon.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateMoonComponent } from './create-moon/create-moon.component';
 import { OneMoonComponent } from './one-moon/one-moon.component';
+import { MoonRoutingModule } from './moon-routing.module';
 
 
 
@@ -11,7 +12,8 @@ import { OneMoonComponent } from './one-moon/one-moon.component';
     OneMoonComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MoonRoutingModule
   ]
 })
 export class MoonModule { }
