6730fb0d7d29ae39cd8481a732c58a71005cb0b5
Simona <simonapalazova@gmail.com>
2023-07-29 20:13:04 +0300

    make moons-list dynamic

--- planets-app/src/app/moon-list/moon-list.component.ts
+++ planets-app/src/app/moon-list/moon-list.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { MoonId } from '../types/moon';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-moon-list',
   templateUrl: './moon-list.component.html',
   styleUrls: ['./moon-list.component.css']
 })
-export class MoonListComponent {
+export class MoonListComponent implements OnInit{
+  moonsList: MoonId[] = [];
+  thereAreNoMoons: boolean = false;
 
+  constructor(
+    private apiServies: ApiService,
+    private userService: UserService
+  ){}
+
+  get isLogged(): boolean{
+    return this.userService.isLogged;
+  }
+
+  ngOnInit(): void {
+    this.apiServies.getMoons().subscribe({
+      next:(moons) => {
+        this.moonsList = moons;
+
+        if(this.moonsList.length === 0){
+          this.thereAreNoMoons = true;
+        }
+      },
+       error: (err) => {
+       console.log(`Error: ${err}`);
+       
+      },
+    })
+  }
 }
