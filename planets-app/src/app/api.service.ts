10c0d497d2919ccc3f99eca7bac489538e31b707
Simona <simonapalazova@gmail.com>
2023-07-29 11:07:58 +0300

    make http req to back-end

--- planets-app/src/app/api.service.ts
+++ planets-app/src/app/api.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { PlanetId } from './types/planet';
+import { MoonId } from './types/moon';
+import { UserId } from './types/user-id';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +12,27 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getUrser(){}
+  getUrser(id: string){
+    const { appUrl } = environment;
+    return this.http.get<UserId>(`${appUrl}/users/${id}/profile`);
+  }
+
+  getPlanets(){
+    const { appUrl } = environment;
+    return this.http.get<PlanetId[]>(`${appUrl}/planets`)
+  }
+
+  getOnePlanet(id: string){
+    const { appUrl } = environment;
+    return this.http.get<PlanetId>(`${appUrl}/planets/${id}`)
+  }
+
+  getMoons(){
+    const { appUrl } = environment;
+    return this.http.get<MoonId[]>(`${appUrl}/moons`)
+  }
+  getOneMoon(id: string){
+    const { appUrl } = environment;
+    return this.http.get<MoonId>(`${appUrl}/moons/${id}`)
+  }
 }
