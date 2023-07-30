c9dff6198cef9d86d920dda98b7197b2829da546
Simona <simonapalazova@gmail.com>
2023-07-30 17:07:28 +0300

    add validations on edit planet form

--- planets-app/src/app/planet/edit-planet/edit-planet.component.ts
+++ planets-app/src/app/planet/edit-planet/edit-planet.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import { appImageValidator } from 'src/app/shared/validators/image-validator';
 
 @Component({
   selector: 'app-edit-planet',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-planet.component.css']
 })
 export class EditPlanetComponent {
+  form = this.fb.group({
+    name:["", [Validators.required]],
+    image:[
+      "",
+      [Validators.required, appImageValidator()],
+    ],
+    planetType:["", [Validators.required]],
+    overview:[
+      "", 
+      [
+        Validators.required, 
+        Validators.minLength(10), 
+        Validators.maxLength(50)
+      ],
+  ]
+  })
+  constructor(private fb: FormBuilder,  private router: Router) {}
+
+  editPlanet():void{
+    if(this.form.invalid){
+      return;
+    }
+    console.log(this.form.value);
+    this.router.navigate(['/planets'])
+    
+  }
 
 }
